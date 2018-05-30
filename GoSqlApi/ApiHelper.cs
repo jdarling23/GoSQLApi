3e640fd82a5f609304de1f73e6714573f9916fa0
James Darling <jdarling@credera.com>
2018-05-30 08:13:19 -0500

    First commit for API

---
+++ GoSqlApi/ApiHelper.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Web.Http;
+
+namespace GoSqlApi
+{
+    public class ApiHelper
+    {
+        #region Properties and Constructor
+
+        private Socket Socket { get; set; }
+        private IPAddress LocalHost { get; set; }
+        private StringBuilder Command { get; set; }
+        private bool HasConnectionError { get; set; }
+        private string ErrorReason { get; set; }
+
+        public ApiHelper()
+        {
+            try
+            {
+                CreateSocket();
+                GetLocalHost();
+                MakeSocketConnection(3333); //Need to find a way abstract this
+                HasConnectionError = false;
+            }
+            catch
+            {
+                HasConnectionError = true;
+            }
+            
+        }
+        #endregion
+
+        #region API Facing Methods
+
+        public string ExecuteCommand(string action, string id, string value = null)
+        {
+            if (HasConnectionError) return "ERROR! " + ErrorReason;
+
+            Command.AppendJoin(" ", action, id);
+            if (value != null) Command.Append(value);
+
+            return TransmitCommand();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void CreateSocket()
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            //Time the socket out if it doesn't recieve a after 3 seconds
+            s.ReceiveTimeout = 3000;
+
+            Socket = s;
+        }
+
+        private void GetLocalHost()
+        {
+            IPAddress[] localIPs = Dns.GetHostAddresses(Dns.GetHostName());
+            LocalHost = localIPs[0]; 
+        }
+
+        private void MakeSocketConnection(int port)
+        {
+            IAsyncResult result = Socket.BeginConnect(LocalHost, port, null, null);
+
+            bool success = result.AsyncWaitHandle.WaitOne(5000, true); //Measured in milliseconds
+
+            if (!Socket.Connected)
+            {              
+                Socket.Close();
+                ErrorReason = "Failed to connect to server.";
+                HasConnectionError = true;
+                throw new ApplicationException();
+            }
+
+        }
+
+        private byte[] EncodeMessage(string message)
+        {
+            return Encoding.UTF8.GetBytes(message);
+        }
+
+        private string DecodeMessage(byte[] message)
+        {
+            return Encoding.UTF8.GetString(message);
+        }
+
+        private string TransmitCommand()
+        {
+            byte[] rawData = new byte[256];
+            string stringData = "";
+
+            try
+            {
+                Socket.Send(EncodeMessage(Command.ToString()));
+                Socket.Receive(rawData);
+            }
+            catch
+            {
+                //Send 500 response with message that connection could not be made
+                Socket.Close();
+                ErrorReason = "Failed to receive response from database.";
+                HasConnectionError = true;
+                throw new ApplicationException();
+            }
+
+            try
+            {
+                stringData = DecodeMessage(rawData);
+            }
+            catch
+            {
+                //Send 500 response that data is corrupted within data base
+                Socket.Close();
+                ErrorReason = "Data my be corrupted within database.";
+                HasConnectionError = true;
+                throw new ApplicationException();
+            }
+
+            Socket.Close();
+            return stringData;
+        }
+
+        #endregion
+    }
+}
---
+++ GoSqlApi/Controllers/DataController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+namespace GoSqlApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class DataController : Controller
+    {
+
+        // GET api/data/var001
+        [HttpGet("{id}")]
+        public string Get(string id)
+        {
+            ApiHelper helper = new ApiHelper();
+            string data = helper.ExecuteCommand("GET", id);
+
+            CheckForErrors(data);
+
+            return data;
+        }
+
+        // POST api/data
+        [HttpPost]
+        public string Post([FromBody]string id, [FromBody]string value)
+        {
+            ApiHelper helper = new ApiHelper();
+            string data = helper.ExecuteCommand("CREATE", id, value);
+
+            CheckForErrors(data);
+
+            return data;
+        }
+
+        // PUT api/data/var001
+        [HttpPut("{id}")]
+        public string Put(string id, [FromBody]string value)
+        {
+            ApiHelper helper = new ApiHelper();
+            string data = helper.ExecuteCommand("UPDATE", id, value);
+
+            CheckForErrors(data);
+
+            return data;
+        }
+
+        // DELETE api/data/var001
+        [HttpDelete("{id}")]
+        public string Delete(string id)
+        {
+            ApiHelper helper = new ApiHelper();
+            string data = helper.ExecuteCommand("DELETE", id);
+
+            CheckForErrors(data);
+
+            return data;
+        }
+
+        #region Error Handling Methods
+        private void CheckForErrors(string data)
+        {
+            if (data.Contains("ERROR!")) ThrowResponseException(data);
+        }
+
+        private void ThrowResponseException(string message)
+        {
+            HttpResponseMessage errorResponse = new HttpResponseMessage(System.Net.HttpStatusCode.InternalServerError)
+            {
+                ReasonPhrase = message
+            };
+            throw new System.Web.Http.HttpResponseException(errorResponse);
+        }
+
+        #endregion
+    }
+}
